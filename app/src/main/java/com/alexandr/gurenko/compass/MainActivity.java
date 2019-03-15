12075900d436c1d9ddfb17b43d1c3fc207177b3b
AlexandrGurenko <gurenko.alexnadr@gmail.com>
2019-03-15 15:25:54 +0200

    added sensor processing

--- app/src/main/java/com/alexandr/gurenko/compass/MainActivity.java
+++ app/src/main/java/com/alexandr/gurenko/compass/MainActivity.java
@@ -1,13 +1,64 @@
 package com.alexandr.gurenko.compass;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView header;
+    private ImageView compass;
+    private float rotateDegree;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        header = findViewById(R.id.header);
+        compass = findViewById(R.id.imgCompass);
+        rotateDegree = 0f;
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        float degree = Math.round(event.values[0]);
+        header.setText("Отклонение от Севера: " + Float.toString(degree) + " градусов");
+
+        RotateAnimation rotateAnimation = new RotateAnimation(rotateDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(200);
+        rotateAnimation.setFillAfter(true);
+
+        compass.startAnimation(rotateAnimation);
+        rotateDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
