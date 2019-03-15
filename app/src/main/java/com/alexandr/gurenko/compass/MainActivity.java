740f77762c72350bf77e14ba21266ec5e5b6c0d0
AlexandrGurenko <gurenko.alexnadr@gmail.com>
2019-03-15 16:08:59 +0200

    change logo

--- app/src/main/java/com/alexandr/gurenko/compass/MainActivity.java
+++ app/src/main/java/com/alexandr/gurenko/compass/MainActivity.java
@@ -1,17 +1,17 @@
 package com.alexandr.gurenko.compass;
 
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends Activity implements SensorEventListener {
 
     private TextView header;
     private ImageView compass;
